06e064132cfd9ca89f132fa1741edf92941a0cc3
TatianaG01 <gorenko_tatiana@knu.ua>
2025-02-23 18:55:14 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,11 @@
-
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    return n * factorial(n-1)
+  def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return True
